9d5651ac23225617c763cb043a30bc6ef2f76e3e
Yondemli <144460650+Yondemli@users.noreply.github.com>
2024-02-28 17:09:28 +0300

    28.02.2024 Update

---
+++ 28.02.2024/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿using WindowsFormsApp1;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.timeEdit1 = new DevExpress.XtraEditors.TimeEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.ImageOptions.Image = global::WindowsFormsApp1.Properties.Resources.save_32x32;
+            this.simpleButton2.Location = new System.Drawing.Point(346, 146);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.simpleButton2.Size = new System.Drawing.Size(194, 35);
+            this.simpleButton2.TabIndex = 4;
+            this.simpleButton2.Text = "Dosyayı Şimdi Kaydet";
+            this.simpleButton2.Click += new System.EventHandler(this.FileUpload);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.ImageOptions.Image = global::WindowsFormsApp1.Properties.Resources.open_32x32;
+            this.simpleButton1.Location = new System.Drawing.Point(346, 94);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.simpleButton1.Size = new System.Drawing.Size(144, 40);
+            this.simpleButton1.TabIndex = 3;
+            this.simpleButton1.Text = "Klasör Seç";
+            this.simpleButton1.Click += new System.EventHandler(this.OpenFolder);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(138, 103);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(191, 22);
+            this.textEdit1.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.FileUpload);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl1.Location = new System.Drawing.Point(138, 373);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(307, 16);
+            this.labelControl1.TabIndex = 8;
+            this.labelControl1.Text = "Son kayıt: ";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(138, 156);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(80, 16);
+            this.labelControl2.TabIndex = 9;
+            this.labelControl2.Text = "Tekrar Sıklığı:";
+            // 
+            // timeEdit1
+            // 
+            this.timeEdit1.EditValue = new System.DateTime(2024, 2, 28, 0, 0, 0, 0);
+            this.timeEdit1.Location = new System.Drawing.Point(252, 149);
+            this.timeEdit1.Name = "timeEdit1";
+            this.timeEdit1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.timeEdit1.Properties.Appearance.Options.UseFont = true;
+            this.timeEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.timeEdit1.Properties.MaskSettings.Set("mask", "t");
+            this.timeEdit1.Properties.TimeEditStyle = DevExpress.XtraEditors.Repository.TimeEditStyle.TouchUI;
+            this.timeEdit1.Size = new System.Drawing.Size(75, 28);
+            this.timeEdit1.TabIndex = 17;
+            this.timeEdit1.EditValueChanged += new System.EventHandler(this.NextSaveTime);
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(135, 190);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(83, 16);
+            this.labelControl3.TabIndex = 18;
+            this.labelControl3.Text = "Sonraki Kayıt: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.timeEdit1);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TimeEdit timeEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+    }
+}
+
---
+++ 28.02.2024/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq.Expressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFolderPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void OpenFolder(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderDialog.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+                {
+                    selectedFolderPath = folderDialog.SelectedPath;
+                    textEdit1.Text = selectedFolderPath;
+                }
+            }
+        }
+        private string GenerateZipFileName(string sourcePath)
+        {
+            string folderName = new DirectoryInfo(sourcePath).Name;
+            System.DateTime now = System.DateTime.Now;
+            string formattedDate = now.ToString("yyyy_MM_dd__HH_mm_ss");
+            return $"{folderName}_{formattedDate}.zip";
+        }
+        private void FileUpload(object sender, EventArgs e)
+        {
+            
+            string sourcePath = selectedFolderPath;
+
+            if (string.IsNullOrWhiteSpace(sourcePath))
+            {
+                MessageBox.Show("Lütfen bir klasör seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string zipFileName = GenerateZipFileName(sourcePath);
+            string targetZipFile = Path.Combine("D:\\TestPlace", zipFileName); //FARKLI BİR BİLGİSAYARDA TEST EDİYORSAN KODU DEĞİŞTİR
+
+            try
+            {
+                if (Directory.Exists(sourcePath))
+                {
+                    ZipFile.CreateFromDirectory(sourcePath, targetZipFile);
+                    labelControl1.Text = "Son kayıt: " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + " Tarihinde alındı.";
+                    //Buraya bir fonksiyon gerekli. Fonksiyonun içeriği timeEdit1'deki veriyi alıp labelControl3.Text'deki yazıyı güncellemeli
+                    MessageBox.Show("Sıkıştırma başarıyla tamamlandı.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void NextSaveTime(object sender, EventArgs e)
+        {
+            TimeSpan selectedDelayTime = timeEdit1.Time.TimeOfDay;
+            DateTime selectedDateTime = DateTime.Now.Add(selectedDelayTime);
+
+            string hourMinute = selectedDelayTime.ToString();
+            string[] hourAndMinute = hourMinute.Split(':');
+            int hour = int.Parse(hourAndMinute[0]);
+            int minute = int.Parse(hourAndMinute[1]);
+
+            TimeSpan interval = new TimeSpan(hour, minute, 0);
+
+            if ((int)interval.TotalMilliseconds != 0)
+            {
+                timer1.Interval = (int)interval.TotalMilliseconds;
+                timer1.Start();
+                labelControl3.Text = "Sonraki Kayıt Zamanı: " + selectedDateTime.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            else
+            {
+                timer1.Stop();
+                labelControl3.Text = "Tekrar sıklığı 0 saniye olamaz.";
+            }
+            
+        }
+    }
+}
